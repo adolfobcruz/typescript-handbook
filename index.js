20de0607480a73f731b6c6038db6c1d30c659332
Adolfo Cruz <adolfo@rewrite.com.br>
2022-06-14 17:42:04 -0400

    main - handbook

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const host = 'localhost';
+const port = 8000;
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('My first server!');
+};
