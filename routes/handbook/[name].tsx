265e54a6f162ee314eae044f2d3e65efceaf65c4
Adolfo Cruz <adolfo@rewrite.com.br>
2022-11-10 09:17:19 -0300

    main - fresh/deno

--- routes/handbook/[name].tsx
+++ routes/handbook/[name].tsx
@@ -1,5 +1,7 @@
 import { PageProps } from "$fresh/server.ts";
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  const { name } = props.params;
+
+  return <div>Hello {name}</div>;
 }
