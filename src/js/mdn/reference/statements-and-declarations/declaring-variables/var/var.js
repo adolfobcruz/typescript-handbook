a3cb361afff7335a0773bd6c3fdc53eeeed5ec1a
Adolfo <adolfo@rewrite.com.br>
2023-12-28 17:46:34 -0400

    main - js/mdn/var

---
+++ src/js/mdn/reference/statements-and-declarations/declaring-variables/var/var.js
@@ -0,0 +1,20 @@
+// Try it
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+
+  console.log(x);
+  // Expected output: 2
+}
+
+console.log(x);
+// Expected output: 2
+
+// Syntax
+var name1;
+var name2 = "value2";
+var name3 = "value3", name4 = "value4";
+var name5, name6 = "value5-6";
+var name6 = "value6", name7 = "value7";
+console.log(name1, name2, name3, name4, name5, name6, name7);
