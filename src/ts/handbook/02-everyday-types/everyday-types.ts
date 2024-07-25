d93ebed132eb10461bf354ffc5a1b379aed7be6d
rwtrecs <adolfo@rewrite.com.br>
2024-07-25 16:20:44 -0300

    main - ts/handbook/everyday-types

--- src/ts/handbook/02-everyday-types/everyday-types.ts
+++ src/ts/handbook/02-everyday-types/everyday-types.ts
@@ -128,3 +128,16 @@
 // printCoord({ x: 100, y: 50 });
 
 // type ID = number | string;
+
+// Interfaces
+interface Point {
+  x: number,
+  y: number,
+}
+
+function printCoord(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 100, y: 50 });
